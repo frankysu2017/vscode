50ee7c135553c21b4a809e8d2d5cffdaec47ef24
frankysu2017 <frankysu@protonmail.com>
2020-04-07 15:46:52 +0800

    master

--- testdb.py
+++ testdb.py
@@ -5,24 +5,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 association_table = db.Table(
-    'association', 
+    'association',
     db.Column('person_id', db.Integer, db.ForeignKey('person.id')),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
-)
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id')))
 
 
 class Person(db.Model):
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     name = db.Column(db.String(255))
-    tags = db.relationship('Tags', 
-    secondary=association_table, 
-    back_populates='persons')
+    tags = db.relationship('Tags',
+                           secondary=association_table,
+                           back_populates='persons')
 
     def __init__(self, name):
         self.name = name
-    
+
     def __repr__(self):
         return "<Person '{}'>".format(self.name)
 
@@ -31,14 +29,15 @@ class Tags(db.Model):
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     tag = db.Column(db.String(255))
     persons = db.relationship('Person',
-    secondary=association_table,
-    back_populates='tags')
+                              secondary=association_table,
+                              back_populates='tags')
 
     def __init__(self, tag):
         self.tag = tag
-    
+
     def __repr__(self):
         return "<Tag '{}'>".format(self.tag)
+
 
 if __name__ == "__main__":
     p1 = Person('Guo')
